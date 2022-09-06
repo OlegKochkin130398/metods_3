a8bcf80249ed928887962c2675dc3f11b9c1cec5
oleg- <o.boys@yandex.ru>
2022-09-06 17:45:45 +0300

    Задание по методам

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void deliveryDaysCheck(int deliveryDistance) {
+        int day = 0;
+        if (deliveryDistance <= 20) {
+            day = 1;
+            System.out.println("Потребуется дней: " + day);
+        }
+        if (deliveryDistance > 20 && deliveryDistance <= 60) {
+            day = 2;
+            System.out.println("Потребуется дней: " + day);
+        }
+        if (deliveryDistance > 60 && deliveryDistance <= 100) {
+            day = 3;
+            System.out.println("Потребуется дней: " + day);
+        }
+    }
+
+    public static void main(String[] args) {
+        int deliveryDistance = 62;
+        deliveryDaysCheck(deliveryDistance);
+    }
 }
